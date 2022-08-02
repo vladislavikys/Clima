5b02176291e70e1a3f5aa219dc4d40d7175c649e
vlad <>
2022-08-02 12:58:24 +0300

    upadte textField ( added cleanup after go)

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,30 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextFiled: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        searchTextFiled.delegate = self
     }
 
-
+    @IBAction func searchPreseed(_ sender: Any) {
+       searchTextFiled.endEditing(true)
+       print(searchTextFiled.text!)
+        // печатем введенны город и удаляем его из TF
+    }
+   
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextFiled.endEditing(true)
+        print(searchTextFiled.text!)
+        return true
+        // печатем введенны город и удаляем его из TF
+    }
 }
 
